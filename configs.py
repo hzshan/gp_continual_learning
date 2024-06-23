71a32a2ae7c9c60b1d0a293544a21c0b5a8a7a50
Haozhe Shan <hzshan@users.noreply.github.com>
2024-06-23 13:46:46 -0400

    updated default student-teacher params to be consistent with those used in paper

--- configs.py
+++ configs.py
@@ -43,15 +43,15 @@ class StudentTeacherArgsParser(cluster_utils.Args):
 
     def __init__(self):
         super().__init__()
-        self.add('P', 50)  # size of each training set
+        self.add('P', 100)  # size of each training set
         self.add('P_test', 100)  # size of each testing set
         self.add('n_tasks', 2, help='number of tasks in the sequence')
         self.add('T', 0.0, help='temperature')
         self.add('sigma', 0.2, help='weight variance')
         self.add('N0', 100, help='input_dimension')
         self.add('Nh', 100, help='hidden layer width of teachers')
-        self.add('NC', 10, help='number of input clusters')
-        self.add('radius', 0.1, help='relative radius of input clusters')
+        self.add('NC', 100, help='number of input clusters')
+        self.add('radius', 0., help='relative radius of input clusters')
         self.add('tsim', 10, help='similarity between teachers, IN PERCENTAGE')
         self.add('xsim', 10, help='similarity between inputs, IN PERCENTAGE')
         self.add('depth', 1,
@@ -62,7 +62,7 @@ class StudentTeacherArgsParser(cluster_utils.Args):
         self.add('N0context', 0, help='embedding dimension')
         self.add('context_strength', 1.0,
                 help='magnifying factor for context embedding')
-        self.add('change_w_in_teachers', 1.0,
+        self.add('change_w_in_teachers', 0.0,
                 help='whether to vary hidden layer weights of teacher NNs')
         self.add('train_data_has_var', 1.0,
                 help='whether training data has deviations from cluster center')
