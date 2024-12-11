af36f85e16a07864871b1b7c56da12939a5897a7
Haozhe Shan <hzshan@users.noreply.github.com>
2024-12-11 10:18:02 -0500

    added code for computing new OPs

--- gp.py
+++ gp.py
@@ -4,16 +4,12 @@ Every random seed corresponds to a different subset of data.
 
 """
 
-
-import numpy as np
-import theory, cluster_utils, data, torch, sys, configs, utils
+import theory, cluster_utils, data, torch, sys, configs
 
 ON_CLUSTER, data_path, output_home_path = cluster_utils.initialize()
 
 parser = configs.GPRealDataArgsParser()
 args = parser.parse_args()
-
-# log whether only saving first task performance
 
 run_name = f'{args.BATCH_NAME}_{args.TRIAL_IND}'
 
@@ -21,12 +17,12 @@ logger = cluster_utils.Logger(
     output_path=f'{output_home_path}{args.BATCH_NAME}/',
     run_name=run_name, only_print=not ON_CLUSTER)
 
-logger.log(str(args))
-results = {'args': args}
+logger.log(str(args))  # log the arguments
+results = {'args': args}  # store results in a dictionary
 
 # Use the same seed for sampling the dataset etc.
 torch.manual_seed(args.seed)
-args.data_seed = args.seed
+args.data_seed = args.seed  # for consistency with other scripts
 
 if args.task_type == 'permuted':
     seq_of_train_x, seq_of_test_x, seq_of_train_y, seq_of_test_y = \
@@ -56,6 +52,7 @@ else:
     raise ValueError(
         'task type not understood. Choose between "permuted" and "split"')
 
+
 # add task embedding (if N0context = 0, it does nothing)
 seq_of_train_x, seq_of_test_x = data.add_task_embedding(
     seq_of_train_x,
@@ -64,6 +61,7 @@ seq_of_train_x, seq_of_test_x = data.add_task_embedding(
     args.context_strength)
 
 
+# compute theory of the mean predictor on all training/test sets
 training_predictions, test_predictions =\
     theory.compute_mean_predictions(
         seq_of_train_x=seq_of_train_x, seq_of_train_y=seq_of_train_y,
@@ -73,12 +71,14 @@ training_predictions, test_predictions =\
         use_naive_gp=(bool(args.naive_gp)))
 
 
+# compute MSE loss and classification accuracy; store in dictionary
 (results['train loss'], results['test loss'],
  results['train acc'], results['test acc']) =\
     data.get_loss_acc(training_predictions,
                       test_predictions,
                       seq_of_train_y,
-                      seq_of_test_y, only_first_task=args.only_first_task)
+                      seq_of_test_y,
+                      only_first_task=args.only_first_task)
 
 
 results['single task test loss'] = theory.get_single_task_test_losses(
@@ -88,6 +88,7 @@ results['single task test loss'] = theory.get_single_task_test_losses(
 
 
 if bool(args.save_outputs):
+    # this saves the exact predictions on the first training set across time.
     # enabling this significantly increases the output file size
 
     results['sampled fn on train'] = training_predictions[0, :]
@@ -104,20 +105,39 @@ if bool(args.save_outputs):
 trp1p2, v1v2_cos, _ = theory.compute_forgetting_ops(
     x1=seq_of_train_x[0], x2=seq_of_train_x[1],
     y1=seq_of_train_y[0], y2=seq_of_train_y[1],
-    depth=args.depth,
-)
+    depth=args.depth)
 
 trp1p2_ntk, v1v2_cos_ntk, _ = theory.compute_forgetting_ops(
     x1=seq_of_train_x[0], x2=seq_of_train_x[1],
     y1=seq_of_train_y[0], y2=seq_of_train_y[1],
-    depth=args.depth, use_ntk_kernel=True
-)
+    depth=args.depth, use_ntk_kernel=True)
+
+dec2024_gamma_rf, dec2024_gamma_r = theory.compute_dec2024_ops(
+    x1=seq_of_train_x[0], x2=seq_of_train_x[1],
+    y1=seq_of_train_y[0], y2=seq_of_train_y[1],
+    depth=args.depth)
+
+def get_gamma_3(x1, x2, y1, depth):
+    k1 = theory.arccos_kernel_deep(x1, x1, depth)
+    k12 = theory.arccos_kernel_deep(x1, x2, depth)
+    k2 = theory.arccos_kernel_deep(x2, x2, depth)
+
+    k1_inv = torch.inverse(k1)
+
+    v_sq_norm = y1.T @ k1_inv @ y1
+
+    return y1.T @ k1_inv @ k12 @ torch.inverse(k2) @ k12.T @ k1_inv @ y1 / v_sq_norm
+
+results['Nov2024 gamma3'] = get_gamma_3(seq_of_train_x[0], seq_of_train_x[1], seq_of_train_y[0], args.depth)
 
 results['V1-V2'] = 2 - 2 * v1v2_cos
 results['tr(P1P2)/P'] = trp1p2
 
 results['V1-V2 ntk'] = 2 - 2 * v1v2_cos_ntk
 results['tr(P1P2)/P ntk'] = trp1p2_ntk
+
+results['Dec2024 gamma_rf'] = dec2024_gamma_rf
+results['Dec2024 gamma_r'] = dec2024_gamma_r
 
 if ON_CLUSTER:
 
