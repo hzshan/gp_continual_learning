3eee5b4e558cd458fcce820328afb88d69e2a229
Haozhe Shan <hzshan@users.noreply.github.com>
2024-12-26 11:58:22 -0500

    updated job submission scripts

--- cluster_scripts/submit_gp_different_P.sh
+++ cluster_scripts/submit_gp_different_P.sh
@@ -1,27 +1,27 @@
 script_name=gp.py
-dataset='fashion'
-task_type='split'
-P=2000
-P_test=100  # for cifar100 this can be at most 200
-n_tasks=5
+dataset='mnist'
+task_type='permuted'
+P_test=1000  # for cifar100 this can be at most 200
+n_tasks=2
 T=0
 sigma=0.2
 resample=0
 manipulation_ratio=1.0  # this argument is ignored for split tasks
 lambda_val=1000000
 use_large_lambda_limit=1  # if 1, ignore lambda_val and assume large lambda
-naive_gp=1  # USING NAIVE GP
+naive_gp=1
 save_outputs=0
 N0context=0
 context_strength=1.0
+depth=1
 whiten=1
 only_first_task=0
 
-MEM_REQUEST=20000 # memory requested, in MB
-TIME_REQUEST=0-1:00
+MEM_REQUEST=10000 # memory requested, in MB
+TIME_REQUEST=0-0:40
 PARTITION=serial_requeue  #or shared
 # batch_name=gp_${n_tasks}x${P}_${dataset}_${task_type}_${N0context}context_${depth}L_diff_strength
-batch_name=gp_${n_tasks}x${P}_${dataset}_${task_type}_diff_depth_whiten_naiveGP
+batch_name=gp_${n_tasks}x${P}_${dataset}_${task_type}_${depth}L_diff_P_naiveGP
 #batch_name=cifar_debug2
 
 
@@ -31,14 +31,13 @@ mkdir $directory
 
 # values=(1 2 3 4 5 6 7 8 9 10)
 # values=(0 25 50 75 100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500)
-values=(1 2 3 5 7 9)
-# values=(1 5 9 13 17)
-seeds=$(seq 1 1 100)
+values=(200 400 600 800 1000 2000 3000 4000 5000 7500 10000)
+seeds=$(seq 1 1 50)
 trial_ind=0
 
-for depth in ${values[@]}
+for P in ${values[@]}
 do
- echo $depth
+ echo $P
  export P P_test n_tasks T sigma depth manipulation_ratio\
  script_name batch_name trial_ind lambda_val task_type dataset manipulation_ratio\
   resample naive_gp save_outputs N0context context_strength\
@@ -47,7 +46,7 @@ do
  for seed in $seeds
  do
    export seed
-   sbatch --account=sompolinsky_lab --job-name=$batch_name --mem=$MEM_REQUEST -t $TIME_REQUEST -p $PARTITION\
+   sbatch --account=cox_lab --job-name=$batch_name --mem=$MEM_REQUEST -t $TIME_REQUEST -p $PARTITION\
    -o /n/home11/haozheshan/ContinualLearning2022/outputs/${batch_name}/run_message_${trial_ind}.txt\
    /n/home11/haozheshan/ContinualLearning2022/cluster_scripts/gp.sbatch.sh
 
--- cluster_scripts/submit_gradient_sim.sh
+++ cluster_scripts/submit_gradient_sim.sh
@@ -1,30 +1,38 @@
+
+# Always check flags in the gradient.py file. 
+
+# The `DIFF_DATA_SEED` flag controls whether different runs use different
+# seeds to sample data.
+
+
 script_name=gradient.py
-P=500
-P_test=500
-n_tasks=20
+P=2000
+P_test=50
+n_tasks=15
 dataset='mnist'
 task_type='permuted'
-depth=3
-permutation=1.0
+depth=1
+manipulation_ratio=0.05
 resample=0
 
-N=1000
+N=2000
 T=0
 sigma=1.0
-eta=0.5
-minibatch=10
-decay=0
+eta=0.1
+minibatch=500
+decay=0.0
 n_epochs=1
 n_steps=500000
-l2=0
+l2=0.1
+whiten=1
 
 
-MEM_REQUEST=7000 # memory requested, in MB
-TIME_REQUEST=0-1:55
+MEM_REQUEST=10000 # memory requested, in MB
+TIME_REQUEST=0-2:00
 PARTITION=seas_gpu
 
-batch_name=gradient_${n_tasks}x${P}_${dataset}_${task_type}_${depth}L_N${N}_MINI${minibatch}_eta${eta}
-#batch_name=cifar_debug2
+batch_name=gradient_${n_tasks}x${P}_${dataset}_${task_type}_full_${depth}L_N${N}_MINI${minibatch}_eta${eta}_diff_data
+# batch_name=${n_tasks}x${P}_${dataset}_${task_type}_full
 
 
 directory="/n/home11/haozheshan/ContinualLearning2022/outputs/${batch_name}/"
@@ -34,7 +42,7 @@ mkdir $directory
 #values=(1 100 500 1000 5000 10000 50000 100000)
 #values=(1000000)
 #values=(1)
-seeds=$(seq 1 1 30)
+seeds=$(seq 1 1 10)
 #values="$(seq 0 0.1 1.57)"
 trial_ind=0
 # values=(400 800 1600 3200)
@@ -44,7 +52,7 @@ trial_ind=0
 echo $batch_name
 echo $lambda_val
 export P P_test n_tasks T sigma depth \
-script_name batch_name trial_ind lambda_val dataset permutation n_epochs l2 N n_steps eta task_type decay minibatch resample
+script_name batch_name trial_ind lambda_val dataset manipulation_ratio n_epochs l2 N n_steps eta task_type decay minibatch resample whiten
 
 for seed in $seeds
 do
